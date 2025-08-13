dfdeb0bb9bfc8134bc6f4bb3ca437662bf1b8ffb
Pieter de Waal <pietergdewaal@gmail.com>
2025-08-13 08:54:28 +0200

    feat(player): add and control idle, run, and jump animations

--- godot-2d-platformer/player/player.gd
+++ godot-2d-platformer/player/player.gd
@@ -10,23 +10,30 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 # movement physics
 func _physics_process(delta):
-	# add grav
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	# add gravity 
+	if not is_on_floor(): # apply when not on floor
+		velocity.y += gravity * delta # increase grav vector
 		
-	# jump
+	# jump on accept button + touching floor
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY # sudden jump velocity
 		
 	# get left/right input
 	# Input.get_axis() returns a value between -1 and 1.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	# left/right movement
-	if direction:
-		velocity.x = direction * SPEED
-	else:
+	if direction: # if dir applied
+		velocity.x = direction * SPEED # add hor speed
+		$AnimatedSprite2D.flip_h = direction < 0 # flip if going left
+		$AnimatedSprite2D.play("run") # animation if going right
+	else: # no left/right input
 		velocity.x = 0 # stop if no left/right
+		$AnimatedSprite2D.play("idle") # idle anim
+		
+	# jump/fall
+	if not is_on_floor(): # if in air
+		$AnimatedSprite2D.play("jump")
 		
 	# func that moves the char
 	move_and_slide()
