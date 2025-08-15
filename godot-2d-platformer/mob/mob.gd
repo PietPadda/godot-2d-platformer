a8131796f496e1e9eed54600d76c455dbdcd7c4f
Pieter de Waal <pietergdewaal@gmail.com>
2025-08-15 19:52:24 +0200

    feat(enemy): add chase AI and disable inter-enemy collision

--- godot-2d-platformer/mob/mob.gd
+++ godot-2d-platformer/mob/mob.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
+# state machine enums
+enum {PATROL, CHASE} # efine our states
+var state = PATROL # start in PATROL
+var player = null # var to hold reference to  player
+
 # ready nodes for use
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var ground_check_ray = $RayCast2D
 
 # variables
-var speed = 80.0 # walk speed
+var speed = 40.0 # walk speed
 var direction: float # only init var
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_stomped = false # death state init
@@ -18,9 +23,22 @@ func _ready():
 	else: # if odd
 		direction = -1.0 # Go left
 
-# mob movement
+# this is our state machine manager
 func _physics_process(delta):
-	# if mob  stomped, STOP all normal movement/animation logic
+	# stomped state
+	if is_stomped:
+		return # early exit
+
+	# state machine
+	match state:
+		PATROL:
+			patrol_state(delta)
+		CHASE:
+			chase_state(delta)
+
+# mob movement (old _physics_process func)
+func patrol_state(delta):
+	# if mob  stomped, STOP patrol logic
 	if is_stomped:
 		return
 	
@@ -47,6 +65,32 @@ func _physics_process(delta):
 		
 	animated_sprite.play("walk") # walk animation
 	move_and_slide() # godots magic func for sliding on floor
+
+# mob chase state
+func chase_state(delta):
+	# if mob  stomped, STOP chase logic
+	if is_stomped:
+		return
+		
+	# apply gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	# edge case
+	# if player disappears, go back to patrolling
+	if not is_instance_valid(player):
+		state = PATROL
+		return
+	
+	# calc direction to player and move towards them
+	# player - mob position to get either + or - value
+	var direction_to_player = (player.global_position - global_position).normalized()
+	velocity.x = direction_to_player.x * speed * 4 # chase a bit faster
+	
+	# standard sprite flip and walk logic
+	animated_sprite.flip_h = velocity.x < 0
+	animated_sprite.play("walk")
+	move_and_slide()
 
 # player lands on enemy head
 func _on_side_detector_body_entered(body: Node2D) -> void:
@@ -79,3 +123,14 @@ func _on_stomp_detector_body_entered(body: Node2D) -> void:
 # func called on timer one shot
 func _on_timer_timeout() -> void:
 	queue_free() # safely delete the mob from  game
+
+# func called on player entering the visibility radius
+func _on_player_detector_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		state = CHASE # set mob to hunt the player
+		player = body # the body it chases is the player
+
+func _on_player_detector_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		state = PATROL # set mob to stop hunting
+		player = null # "forget" about the player as it left vis circle
