54c3cf47ec9d4867ee806f8fbeb4a4834ca28302
Pieter de Waal <pietergdewaal@gmail.com>
2025-08-15 23:07:07 +0200

    feat(game): create main menu and set as new entry point

---
+++ godot-2d-platformer/ui/main_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+# path to main game level
+const GAME_SCENE_PATH = "res://main.tscn"
+
+# press Play Game button
+func _on_button_pressed() -> void:
+	# tell scene tree to switch from menu to main game scene
+	get_tree().change_scene_to_file(GAME_SCENE_PATH)
