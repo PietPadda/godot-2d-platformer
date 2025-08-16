22be807099c812a40c78e95196692c61b7333e10
Pieter de Waal <pietergdewaal@gmail.com>
2025-08-16 12:28:25 +0200

    feat(player): implement double jump mechanic

--- godot-2d-platformer/player/player.gd
+++ godot-2d-platformer/player/player.gd
@@ -1,3 +1,5 @@
+# player/player.gd
+
 # expose vars & funcs
 extends CharacterBody2D
 
@@ -8,15 +10,23 @@ const JUMP_VELOCITY = -450.0 # float
 # get global grav for rigidbody
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# variables
+var jumps_left = 0 # init no jumps
+
 # movement physics
 func _physics_process(delta):
 	# add gravity 
 	if not is_on_floor(): # apply when not on floor
 		velocity.y += gravity * delta # increase grav vector
+	
+	# reset jumps if on floor
+	if is_on_floor():
+		jumps_left = 2 # double jump
 		
-	# jump on accept button + touching floor
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# jump on accept button + have jumps left
+	if Input.is_action_just_pressed("ui_accept") and jumps_left > 0:
 		velocity.y = JUMP_VELOCITY # sudden jump velocity
+		jumps_left -= 1 # decr jumps
 		
 	# get left/right input
 	# Input.get_axis() returns a value between -1 and 1.
