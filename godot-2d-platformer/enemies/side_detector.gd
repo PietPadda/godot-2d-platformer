39556cd8713127188c5108b26700c221dab21c1a
Pieter de Waal <pietergdewaal@gmail.com>
2025-08-20 08:00:56 +0200

    feat(enemy): added player and parent tracking for timer application

--- godot-2d-platformer/enemies/side_detector.gd
+++ godot-2d-platformer/enemies/side_detector.gd
@@ -5,9 +5,19 @@ extends Area2D
 # ready nodes
 @onready var damage_timer = $DamageTimer
 
+# script scope vars
+var player_body = null # init player reference
+var parent_body = null # init parent reference
+
+# initialisation at scene start
+func _ready():
+	# get ref to parent node (BaseEnemy) when scene starts
+	parent_body = get_parent() # parent refer
+
 # player enter side detector
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not parent_body.is_stomped:
+		player_body = body # player reference
 		GameEvents.deal_damage_to_player.emit(1) # deal damage
 		damage_timer.start() # start dmg timer
 
@@ -18,4 +28,5 @@ func _on_body_exited(body: Node2D) -> void:
 
 # damage timer tick
 func _on_damage_timer_timeout() -> void:
-	GameEvents.deal_damage_to_player.emit(1) # deal damage per tick
+	if is_instance_valid(player_body) and not parent_body.is_stomped:
+		GameEvents.deal_damage_to_player.emit(1) # deal damage per tick
